2424dd9be1e05783fca695283a688b29d432854e
Md. Golam Kibria <78980142+kibria1010@users.noreply.github.com>
2024-11-09 14:15:23 +0600

    Add files via upload

--- Iinheritance.java
+++ Iinheritance.java
@@ -1,14 +1,7 @@
 package oop;
 
-/**
- * Inheritance in Java is a key object-oriented programming concept that allows
- * a class (called a subclass or derived class) to inherit fields and methods
- * from another class (called a superclass or base class). This enables code
- * reusability, supports polymorphism, and establishes a hierarchical
- * relationship between classes.
- */
 public class Iinheritance {
-
+    
     public static void main(String[] args) {
         Toyota myCar = new Toyota("Toyota");
         myCar.start();
@@ -67,3 +60,41 @@ class Toyota extends Car {
     }
 
 }
+
+/*
+Inheritance:
+    Inheritance in Java is a key object-oriented programming concept that allows
+    a class (called a subclass or derived class) to inherit fields and methods
+    from another class (called a superclass or base class). 
+    This enables code reusability, supports polymorphism, and establishes a hierarchical relationship between classes.
+
+Types of Inheritance:
+    Java supports the following types of inheritance:
+    Single inheritance: A class can inherit from only one superclass. Java does not support multiple inheritance with classes, to avoid ambiguity.
+    Multilevel inheritance: A chain of inheritance where a class is derived from another derived class.
+    Note:
+
+super Keyword:
+    The super keyword allows access to the superclass's methods and constructors from the subclass. It's often used to:
+    Invoke the superclass's constructor is must at first line.
+    Call methods from the superclass that are overridden in the subclass.
+
+Method Overriding:
+    When a subclass defines a method with the same signature as a method in its superclass, it "overrides" the superclass method. This allows the subclass to provide specific behavior for that method.
+
+Access Modifiers
+    Public members are accessible in all classes.
+    Protected members are accessible in subclasses and within the same package.
+    Default (no modifier) members are accessible within the same package only.
+    Private members are not accessible in the subclass directly but can be accessed through public or protected getters/setters if provided.
+
+Advantages of Inheritance
+    Code Reusability: Shared code can be placed in a superclass and reused by subclasses.
+    Polymorphism: Allows treating instances of different subclasses as instances of a superclass, making code more flexible.
+    Extensibility: New functionalities can be added by creating subclasses without modifying existing code.
+
+Key Rules
+    -Java does not support multiple inheritance with classes to avoid complexity and ambiguity.
+    -Constructors are not inherited but can be accessed with super.
+    -Private members are not directly accessible by subclasses.
+*/
