23330d519b3bbd15268579a51d7a2faf53f7295b
Md. Golam Kibria <78980142+kibria1010@users.noreply.github.com>
2024-11-09 14:46:33 +0600

    Add files via upload

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,7 +1,8 @@
 package oop;
 
 /**
- * Polymorphism is an important concept in Java that allows objects to be treated as instances of their superclass, enabling multiple forms of behavior based on the object's actual type.
+ * Polymorphism is an important concept in Java that allows objects to be treated as instances of their superclass, 
+ * enabling multiple forms of behavior based on the object's actual type.
 */
 
 public class Polymorphism {
